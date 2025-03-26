c0da52a8c91bf28fca77b29b7fcd074483b25652
Jeffrey Ju <jeffreyju1899@gmail.com>
2025-03-26 10:32:25 -0700

    access key structure update

--- backend/util/secretKey.js
+++ backend/util/secretKey.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const ssmClient = new SSMClient({ 
     region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: process.env.ACCESS_KEY_ID,
-        secretAccessKey: process.env.SECRET_ACCESS_KEY
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     }
 });
 
